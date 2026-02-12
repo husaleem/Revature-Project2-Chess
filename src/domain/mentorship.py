4b0c970a17505d075d117a72c1a4db9843d50107
josef923 <josef923@revature.net>
2026-02-12 15:12:19 -0600

    Added mentorship.py domain

---
+++ src/domain/mentorship.py
@@ -0,0 +1,19 @@
+import uuid
+from sqlalchemy import Column, ForeignKey
+from sqlalchemy.dialects.postgresql import UUID
+from src.base import Base
+
+
+class Mentorship(Base):
+    __tablename__ = "mentors"
+
+    player_id = Column(
+        UUID(as_uuid=True), ForeignKey("players.player_id"), primary_key=True
+    )
+    mentor_id = Column(
+        UUID(as_uuid=True), ForeignKey("players.player_id"), primary_key=True
+    )
+
+    def set_mentorship(self, player_id, mentor_id):
+        self.player_id = player_id
+        self.mentor_id = mentor_id
