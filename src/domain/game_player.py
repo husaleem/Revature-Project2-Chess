241be9c636f2a718f5f83b90cdc15f1f20a3c3a1
Yurii Koval <30389972+ykoval201@users.noreply.github.com>
2026-02-13 15:10:47 -0600

    Fixed small typo with Column(Integer) -> Column(UUID)

--- src/domain/game_player.py
+++ src/domain/game_player.py
@@ -1,3 +1,4 @@
+from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy import Column, Integer, String, ForeignKey
 from src.base import Base
 
@@ -5,7 +6,7 @@ from src.base import Base
 class GamePlayer(Base):
     __tablename__ = "game_player"
 
-    game_id = Column(Integer, ForeignKey("games.game_id"), primary_key=True)
-    player_id = Column(Integer, ForeignKey("players.player_id"), primary_key=True)
+    game_id = Column(UUID(as_uuid=True), ForeignKey("games.game_id"), primary_key=True)
+    player_id = Column(UUID(as_uuid=True), ForeignKey("players.player_id"), primary_key=True)
     color = Column(String(10), nullable=False)
     result = Column(String(10), nullable=False)
